7dca3cad691eb19aa3d67dd881ec14b841beef07
karp2601 <karp2601@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2023-10-26 16:04:43 -0400

    WIP

--- scripts/combine_plots.py
+++ scripts/combine_plots.py
@@ -1,16 +1,10 @@
 import argparse
-import nibabel as nib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 
-from modules.io import (plot_means, plot_3d_means, plot_multiple_means,
-                        save_angle_maps, save_masks_by_angle_bins,
-                        save_results_as_npz, extract_measures)
-from modules.orientation_dependence import (analyse_delta_m_max,
-                                            compute_three_fibers_means,
-                                            compute_two_fibers_means,
-                                            compute_single_fiber_means,
-                                            fit_single_fiber_results)
+from modules.io import plot_init
 
 from scilpy.io.utils import (add_overwrite_arg)
 
@@ -25,11 +19,14 @@ def _build_arg_parser():
     p.add_argument('--results', nargs='+', default=[],
                    action='append', required=True,
                    help='List of characterization results.')
-    p.add_argument('--bundle_names', nargs='+', default=[], action='append',
+    p.add_argument('--bundles_names', nargs='+', default=[], action='append',
+                   required=True,
                    help='List of names for the characterized bundles.')
     p.add_argument('--polyfits', nargs='+', default=[],
-                   action='append',
+                   action='append', required=True,
                    help='List of polyfits.')
+    p.add_argument('--measures_names', nargs='+', default=[], action='append',
+                   help='List of names the characterized measures.')
 
     g = p.add_argument_group(title='Characterization parameters')
     g.add_argument('--min_nb_voxels', default=30, type=int,
@@ -44,18 +41,54 @@ def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
-    nb_measures = len(args.measures[0])
-    if args.measures_names != [] and\
-        (len(args.measures_names[0]) != nb_measures):
-        parser.error('When using --measures_names, you need to specify ' +
-                     'the same number of measures as given in --measures.')
+    nb_results = len(args.results[0])
+    if args.bundles_names != [] and (len(args.bundles_names[0]) != nb_results):
+        parser.error('When using --bundles_names, you need to specify ' +
+                     'the same number of bundles as given in --results.')
 
-    if args.measures_corrected != [] and\
-        (len(args.measures_corrected[0]) != nb_measures):
-        parser.error('When using --measures_corrected, you need to specify ' +
-                     'the same number of measures as given in --measures.')
+    if args.polyfits != [] and (len(args.polyfits[0]) != nb_results):
+        parser.error('When using --polyfits, you need to specify ' +
+                     'the same number of polyfits as given in --results.')
 
     out_folder = Path(args.out_folder)
+
+    results = []
+    bundles_name = []
+    polyfits = []
+    for i, result in enumerate(args.results[0]):
+        results.append(np.load(result))
+        bundles_name.append(args.bundles_names[0][i])
+        polyfits.append(np.load(result))
+
+    plot_init()
+
+    max_count = np.max(nb_voxels)
+    norm = mpl.colors.Normalize(vmin=0, vmax=max_count)
+    mid_bins = (bins[:-1] + bins[1:]) / 2.
+    highres_bins = np.arange(0, 90 + 1, 0.5)
+    plot_init()
+    for i in range(means.shape[-1]):
+        out_path = out_folder / str("original_" + str(names[i]) + "_1f.png")
+        fig, (ax1, cax) = plt.subplots(1, 2,
+                                       gridspec_kw={"width_ratios":[1, 0.05]})
+        colorbar = ax1.scatter(mid_bins, means[..., i], c=nb_voxels,
+                               cmap='Greys', norm=norm,
+                               edgecolors="C0", linewidths=1)
+        if cr_means is not None:
+            ax1.scatter(mid_bins, cr_means[..., i], c=nb_voxels, cmap='Greys',
+                        norm=norm, edgecolors="C0", linewidths=1, marker="s")
+            out_path = out_folder / str("corrected_" + str(names[i]) + "_1f.png")
+        if polyfit is not None:
+            polynome = np.poly1d(polyfit[:, i])
+            ax1.plot(highres_bins, polynome(highres_bins), "--", color="C0")
+        ax1.set_xlabel(r'$\theta_a$')
+        ax1.set_xlim(0, 90)
+        ax1.set_ylim(0.975 * np.nanmin(means[..., i]), 1.025 * np.nanmax(means[..., i]))
+        ax1.set_ylabel(str(names[i]) + ' mean')
+        fig.colorbar(colorbar, cax=cax, label="Voxel count")
+        fig.tight_layout()
+        plt.savefig(out_path, dpi=300)
+        plt.close()
 
 
 if __name__ == "__main__":
