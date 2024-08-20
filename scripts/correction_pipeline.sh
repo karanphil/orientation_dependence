401c974ff3b74cc36c7f8fa0e124602685ed1cb8
Philippe Karan <philippe.karan@usherbrooke.ca>
2024-08-20 07:37:35 -0400

    WIP

--- scripts/correction_pipeline.sh
+++ scripts/correction_pipeline.sh
@@ -12,9 +12,9 @@ do_filter_trk=false;
 do_sift2=false;
 do_bundles=false;
 do_fixel_density=false;
-do_characterize_original=true;
-do_plot_original=true;
-do_correction=true;
+do_characterize_original=false;
+do_plot_original=false;
+do_correction=false;
 do_characterize_corrected=true;
 do_plot_corrected=true;
 do_tractometry=false;
@@ -192,7 +192,7 @@ if $do_characterize_corrected;
 
     python ${source}/orientation_dependence/scripts/scil_orientation_dependence_characterization.py FODF_metrics/${data}/new_peaks/peaks.nii.gz FODF_metrics/${data}/new_peaks/peak_values.nii.gz DTI_metrics/${data}/${data}__dti_fa.nii.gz FODF_metrics/${data}/new_peaks/nufo.nii.gz wm_mask/${data}/${data}__wm_mask.nii.gz $out_corrected --measures ihMT/${data}/MTR_corrected.nii.gz ihMT/${data}/ihMTR_corrected.nii.gz ihMT/${data}/MTsat_corrected.nii.gz ihMT/${data}/ihMTsat_corrected.nii.gz --measures_names MTR ihMTR MTsat ihMTsat --bundles $bundles_masks --bundles_names $bundles_names --bin_width_sf $bin_width --min_nb_voxels 1 --min_frac_pts 0.75 --stop_crit 0.06 --save_polyfit --use_weighted_polyfit;
 
-    mkdir -p ${out_corrected}/${data}/WM;
+    mkdir -p ${out_corrected}/WM;
     python ${source}/orientation_dependence/scripts/scil_orientation_dependence_characterization.py FODF_metrics/${data}/new_peaks/peaks.nii.gz FODF_metrics/${data}/new_peaks/peak_values.nii.gz DTI_metrics/${data}/${data}__dti_fa.nii.gz FODF_metrics/${data}/new_peaks/nufo.nii.gz wm_mask/${data}/${data}__wm_mask.nii.gz $out_corrected --measures ihMT/${data}/MTR_corrected.nii.gz ihMT/${data}/ihMTR_corrected.nii.gz ihMT/${data}/MTsat_corrected.nii.gz ihMT/${data}/ihMTsat_corrected.nii.gz --measures_names MTR ihMTR MTsat ihMTsat --bundles ${fixel_analysis}/voxel_density_mask_WM.nii.gz --bundles_names WM --bin_width_sf $bin_width --min_nb_voxels 1 --stop_crit 0.06 --save_polyfit --use_weighted_polyfit;
 
 fi;
