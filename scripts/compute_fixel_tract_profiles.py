7093ab4ae4fd26ef6fdf383f8d753d0959d5d228
Philippe Karan <philippe.karan@usherbrooke.ca>
2024-09-25 17:06:37 -0400

    WIP

--- scripts/compute_fixel_tract_profiles.py
+++ scripts/compute_fixel_tract_profiles.py
@@ -26,6 +26,10 @@ def _build_arg_parser():
                    help='')
     p.add_argument('--bundles_names', nargs='+', default=[],
                    help='List of names for the bundles.')
+
+    p.add_argument('--in_weights', required=True,
+                   help='')
+
     p.add_argument('--type', type=str)
 
     add_overwrite_arg(p)
@@ -48,11 +52,15 @@ def main():
 
     df = pd.read_csv(args.in_csv)
 
+    df = df[df['Sid'] == 'sub-026-hc_ses-3']
+
     data_shape = (nib.load(args.measures[0])).get_fdata().shape
 
     measures, measures_name = extract_measures(args.measures,
                                                data_shape,
                                                args.measures_names)
+
+    measures[measures == 0] = None
 
     bundles_labels = []
     bundles_names = []
@@ -67,10 +75,14 @@ def main():
     nb_measures = measures.shape[-1]
 
     if nb_bundles != measures.shape[-2]:
-        parser.error('The number of bundles in the measures does not ' +
-                     'correspond to the bundles labels.')
+        measures = np.repeat(measures[:, :, :, np.newaxis, :], nb_bundles, axis=3)
+        # parser.error('The number of bundles in the measures does not ' +
+        #              'correspond to the bundles labels.')
 
     nb_labels = int(np.nanmax(bundles_labels[0]) - np.nanmin(bundles_labels[0]))
+
+    density_weights = nib.load(args.in_weights).get_fdata()
+    density_weights = np.sum(density_weights, axis=-2)
 
     mean_profiles = np.zeros((nb_bundles, nb_labels, nb_measures))
     std_profiles = np.zeros((nb_bundles, nb_labels, nb_measures))
@@ -80,14 +92,24 @@ def main():
         print(measures_name[k])
         for i, bundle in enumerate(bundles_labels):
             print(bundles_names[i])
-            means[i, k] = np.nanmean(measures[..., i, k][bundle >= 1])
-            stds[i, k] = np.nanstd(measures[..., i, k][bundle >= 1])
+            masked_data = np.ma.masked_array(measures[..., i, k][bundle >= 1],
+                                             np.isnan((measures[..., i, k][bundle >= 1])))
+            means[i, k] = np.average(masked_data, weights=density_weights[..., i][bundle >= 1])
+            stds[i, k] = np.sqrt(np.average((masked_data - means[i, k]) ** 2, weights=density_weights[..., i][bundle >= 1]))
+            # means[i, k] = np.nanmean((measures[..., i, k] * density_weights[... , i])[bundle >= 1])
+            # stds[i, k] = np.nanstd((measures[..., i, k] * density_weights[..., i])[bundle >= 1])
             df.loc[(df['Statistics'] == 'mean') & (df['Measures'] == measures_name[k]) & (df['Type'] == args.type) & (df['Bundles'] == bundles_names[i]) & (df['Section'].isnull()), ['Value']] = means[i, k]
             df.loc[(df['Statistics'] == 'std') & (df['Measures'] == measures_name[k]) & (df['Type'] == args.type) & (df['Bundles'] == bundles_names[i]) & (df['Section'].isnull()), ['Value']] = stds[i, k]
             for j in range(nb_labels):
                 print("Section ", j + 1)
-                mean_profiles[i, j, k] = np.nanmean(measures[..., i, k][bundle == j + 1])
-                std_profiles[i, j, k] = np.nanstd(measures[..., i, k][bundle == j + 1])
+                masked_data = np.ma.masked_array(measures[..., i, k][bundle == j + 1],
+                                             np.isnan((measures[..., i, k][bundle == j + 1])))
+                mean_profiles[i, j, k] = np.average(masked_data,
+                                                    weights=density_weights[..., i][bundle == j + 1])
+                std_profiles[i, j, k] = np.sqrt(np.average((masked_data - mean_profiles[i, j, k]) ** 2,
+                                                           weights=density_weights[..., i][bundle == j + 1]))
+                # mean_profiles[i, j, k] = np.nanmean((measures[..., i, k] * density_weights[..., i])[bundle == j + 1])
+                # std_profiles[i, j, k] = np.nanstd((measures[..., i, k] * density_weights[..., i])[bundle == j + 1])
                 df.loc[(df['Statistics'] == 'mean') & (df['Measures'] == measures_name[k]) & (df['Type'] == args.type) & (df['Bundles'] == bundles_names[i]) & (df['Section'] == j + 1), ['Value']] = mean_profiles[i, j, k]
                 df.loc[(df['Statistics'] == 'std') & (df['Measures'] == measures_name[k]) & (df['Type'] == args.type) & (df['Bundles'] == bundles_names[i]) & (df['Section'] == j + 1), ['Value']] = std_profiles[i, j, k]
 
