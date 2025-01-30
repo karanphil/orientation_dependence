73b4db571bfcb7799d7424aca6f951972ddb0379
Philippe Karan <philippe.karan@usherbrooke.ca>
2025-01-30 16:29:58 -0500

    Finishing new script

--- modules/orientation_dependence.py
+++ modules/orientation_dependence.py
@@ -355,8 +355,7 @@ def compute_single_fiber_means_new(peaks, fa, wm_mask, affine,
     return bins, measure_means, measure_stds, nb_voxels
 
 
-def compute_fiber_means_from_mask(peaks, mask, affine, measures, bin_width=1,
-                                  fa_thr=0.5):
+def compute_fiber_means_from_mask(peaks, mask, affine, measures, bin_width=1):
     # Find the direction of the B0 field
     rot = affine[0:3, 0:3]
     z_axis = np.array([0, 0, 1])
@@ -365,26 +364,18 @@ def compute_fiber_means_from_mask(peaks, mask, affine, measures, bin_width=1,
     bins = np.arange(0, 90 + bin_width, bin_width)
 
     # Calculate the angle between e1 and B0 field
-    cos_theta = np.dot(peaks[..., :3], b0_field)
+    cos_theta = np.dot(peaks, b0_field)
     theta = np.arccos(cos_theta) * 180 / np.pi
 
     measure_means = np.zeros((len(bins) - 1, measures.shape[-1]))
     measure_stds = np.zeros((len(bins) - 1, measures.shape[-1]))
     nb_voxels = np.zeros((len(bins) - 1, measures.shape[-1]))
 
-    # Apply the WM mask and FA threshold
-    if nufo is not None:
-        wm_mask_bool = (wm_mask >= 0.9) & (fa > fa_thr) & (nufo == 1)
-    else:
-        wm_mask_bool = (wm_mask >= 0.9) & (fa > fa_thr)
-    if mask is not None:
-        wm_mask_bool = wm_mask_bool & (mask > 0)
-
     for i in range(len(bins) - 1):
         angle_mask_0_90 = (theta >= bins[i]) & (theta < bins[i+1]) 
         angle_mask_90_180 = (180 - theta >= bins[i]) & (180 - theta < bins[i+1])
         angle_mask = angle_mask_0_90 | angle_mask_90_180
-        mask_total = wm_mask_bool & angle_mask
+        mask_total = mask > 0 & angle_mask
         nb_voxels[i, :] = np.sum(mask_total)
         if np.sum(mask_total) < 1:
             measure_means[i, :] = None
--- scripts/scil_orientation_dependence_characterization_multi_fibers.py
+++ scripts/scil_orientation_dependence_characterization_multi_fibers.py
@@ -7,12 +7,8 @@ from pathlib import Path
 
 from scilpy.io.utils import add_verbose_arg
 
-from modules.io import (save_results_as_npz, extract_measures,
-                        save_polyfits_as_npz)
-from modules.orientation_dependence import (compute_single_fiber_means_new,
-                                            fit_single_fiber_results_new,
-                                            where_to_patch,
-                                            patch_measures)
+from modules.io import (save_results_as_npz, extract_measures)
+from modules.orientation_dependence import compute_fiber_means_from_mask
 
 
 def _build_arg_parser():
@@ -48,9 +44,6 @@ def _build_arg_parser():
     g.add_argument('--bin_width_mf', default=10, type=int,
                    help='Value of the bin width for the multi-fiber '
                         'characterization [%(default)s].')
-    g.add_argument('--min_frac_thr', default=0.1,
-                   help='Value of the minimal fraction threshold for '
-                        'selecting peaks to correct [%(default)s].')
     g.add_argument('--min_nb_voxels', default=1, type=int,
                    help='Value of the minimal number of voxels per bin '
                         '[%(default)s].')
@@ -101,6 +94,8 @@ def main():
     measure_stds = np.zeros((nb_bundles, nb_bins, nb_measures))
     nb_voxels = np.zeros((nb_bundles, nb_bins, nb_measures))
     is_measures = np.ndarray((nb_bundles, nb_bins), dtype=bool)
+    pts_origin = np.ndarray((nb_bundles, nb_bins, nb_measures), dtype=object)
+    pts_origin.fill("None")
 
     # For every bundle, compute the mean measures
     for i, bundle_name in enumerate(bundles_names):
@@ -112,62 +107,38 @@ def main():
                     raise ValueError("Polyfit from bundle not present in lookup table.")
         else:
             bundle_idx = i
-        mf_bunlde_mask = np.where(np.sum(fixel_density_masks[..., bundle_idx], axis=-1) >= 1, 1, 0)
-        # print(fixel_density_masks[..., bundle_idx])
-        peak_index = np.argwhere(fixel_density_masks[..., bundle_idx] == 1)[..., 0]
-        bundle_peaks = peaks[peak_index]
-    #     bins, measure_means[i], measure_stds[i], nb_voxels[i] =\
-    #         compute_single_fiber_means_new(peaks, fa,
-    #                                        wm_mask,
-    #                                        affine,
-    #                                        measures,
-    #                                        nufo=nufo,
-    #                                        mask=bundle,
-    #                                        bin_width=args.bin_width_sf,
-    #                                        fa_thr=args.fa_thr)
+        first_peak_index = np.argmax(fixel_density_masks[..., bundle_idx],
+                                     axis=3) * 3
+        indices_to_select = np.stack([first_peak_index, first_peak_index + 1,
+                                      first_peak_index + 2], axis=-1)
+        bundle_peaks = np.take_along_axis(peaks, indices_to_select, axis=3)
+        mf_bundle_mask = np.where(np.sum(fixel_density_masks[..., bundle_idx],
+                                         axis=-1) >= 1, 1, 0)
+        bins, measure_means[i], measure_stds[i], nb_voxels[i] =\
+            compute_fiber_means_from_mask(bundle_peaks,
+                                          mf_bundle_mask,
+                                          affine,
+                                          measures,
+                                          bin_width=args.bin_width_mf)
+        pts_origin[i, is_measures[i]] = bundle_name
+        is_measures[i] = nb_voxels[i, :, 0] >= min_nb_voxels
+        nb_filled_bins = np.sum(is_measures[i])
+        if nb_filled_bins == 0:
+            msg = """No angle bin was filled above the required minimum number
+                     of voxels. The script was unable to produce a single-fiber
+                     characterization of the measures. If --bundles was used,
+                     the region of interest probably contains too few
+                     single-fiber voxels. Try to carefully reduce the
+                     min_nb_voxels."""
+            raise ValueError(msg)
 
-    #     is_measures[i] = nb_voxels[i, :, 0] >= min_nb_voxels
-    #     pts_origin[i, is_measures[i]] = bundle_name
-    #     nb_filled_bins = np.sum(is_measures[i])
-    #     if nb_filled_bins == 0:
-    #         msg = """No angle bin was filled above the required minimum number
-    #                  of voxels. The script was unable to produce a single-fiber
-    #                  characterization of the measures. If --bundles was used,
-    #                  the region of interest probably contains too few
-    #                  single-fiber voxels. Try to carefully reduce the
-    #                  min_nb_voxels."""
-    #         raise ValueError(msg)
-    #     averages[i] = np.ma.average(np.ma.MaskedArray(measure_means[i],
-    #                                                   mask=np.isnan(measure_means[i])),
-    #                                 weights=nb_voxels[i], axis=0)
+    # Saving the results of orientation dependence characterization
+    for i in range(nb_bundles):
+        out_path = out_folder / (bundles_names[i] + '/1f_results')
+        save_results_as_npz(bins, measure_means[i], measure_stds[i],
+                            nb_voxels[i], pts_origin[i], measures_name,
+                            out_path)
 
-    # # Saving the results of orientation dependence characterization
-    # for i in range(nb_bundles):
-    #     out_path = out_folder / (bundles_names[i] + '/1f_results')
-    #     save_results_as_npz(bins, measure_means[i], measure_stds[i], nb_voxels[i],
-    #                         pts_origin[i], measures_name, out_path)
-
-    # new_is_measures = nb_voxels >= min_nb_voxels
-
-    # # For every bundle, fit the polynome
-    # if args.save_polyfit:
-    #     for i, (bundle, bundle_name) in enumerate(zip(bundles, bundles_names)):
-    #         logging.info("Fitting the results of bundle {}.".format(bundle_name))
-    #         # Taking the average of the mean points instead of all the points
-    #         # averages[i] = np.nanmean(measure_means[i], axis=0)
-    #         measures_fit, measures_max = fit_single_fiber_results_new(bins,
-    #                                                     measure_means[i],
-    #                                                     is_measures=new_is_measures[i],
-    #                                                     nb_voxels=nb_voxels[i],
-    #                                                     stop_crit=args.stop_crit,
-    #                                                     use_weighted_polyfit=args.use_weighted_polyfit)
-    #         out_path = out_folder / (bundles_names[i] + '/1f_polyfits')
-    #         if args.reference == "mean":
-    #             save_polyfits_as_npz(measures_fit, averages[i],
-    #                                  measures_name, out_path)
-    #         elif args.reference == "maximum":
-    #             save_polyfits_as_npz(measures_fit, measures_max,
-    #                                  measures_name, out_path)
 
 
 if __name__ == "__main__":
