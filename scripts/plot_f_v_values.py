e32eb8fefecd47424d8eeecf3392d38513051961
Philippe Karan <philippe.karan@usherbrooke.ca>
2025-03-22 14:33:50 -0400

    WIP

--- scripts/plot_f_v_values.py
+++ scripts/plot_f_v_values.py
@@ -198,15 +198,7 @@ def main():
         plt.savefig(args.out_filename, dpi=500, bbox_inches='tight')
         plt.close()
     elif is_mf_values and args.tractometry:
-        # c = [0, 0, 1, 1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 13, 13, 13, 13, 14, 14, 15]
         symbols = ["o", "s", "^"]
-        # values_mf[:, 0] /= np.max(values_mf[:, 0])
-        # values_mf[:, 1] /= np.max(values_mf[:, 1])
-        # values_mf[:, 2] /= np.max(values_mf[:, 2])
-        # values_mf[:, 3] /= np.max(values_mf[:, 3])
-        # values_mf[:, 1] += 0.5
-        # values_mf[:, 2] += 1
-        # values_mf[:, 3] += 1.5
         diffs = np.zeros((2,) + (values_mf.shape[1],) + (values_mf.shape[2],))
         diffs[0] = (values_mf[1] - values_mf[0]) / values_mf[0]
         diffs[1] = (values_mf[2] - values_mf[0]) / values_mf[0]
@@ -271,6 +263,42 @@ def main():
                         markerfacecolor=cmap(cmap_idx[0]), linestyle='')
         ax[1].legend(handles=[point1, point2], title="Reference", loc=(0.6, 0.1), handletextpad=0.1)
         # ax[1].set_xlim(0, 16)
+        # plt.show()
+        # plt.subplots_adjust(wspace=2)
+        plt.savefig(args.out_filename, dpi=500, bbox_inches='tight')
+        plt.close()
+    elif not is_mf_values and  args.tractometry:
+        symbols = ["o", "s", "^"]
+        diffs = np.zeros((2,) + (values_sf.shape[1],) + (values_sf.shape[2],))
+        diffs[0] = (values_sf[1] - values_sf[0]) / values_sf[0]
+        diffs[1] = (values_sf[2] - values_sf[0]) / values_sf[0]
+        diffs[:, 1, :] += 0.5
+        diffs[:, 2, :] += 1
+        diffs[:, 3, :] += 1.5
+        fig, ax = plt.subplots(1, 1, layout='constrained')
+        ax.set_xlim(-1, 32 * 3 + 1)
+        ax.hlines(0, -1, 32 * 3 + 1, linestyles="dashed", colors="grey", alpha=0.5, linewidth=1)
+        ax.hlines(0.5, -1, 32 * 3 + 1, linestyles="dashed", colors="grey", alpha=0.5, linewidth=1)
+        ax.hlines(1, -1, 32 * 3 + 1, linestyles="dashed", colors="grey", alpha=0.5, linewidth=1)
+        ax.hlines(1.5, -1, 32 * 3 + 1, linestyles="dashed", colors="grey", alpha=0.5, linewidth=1)
+        for i in range(nb_sets - 1):
+            for j in range(values_sf[i].shape[-1]):
+                ax.scatter([3*j, 3*j, 3*j, 3*j], diffs[i, :, j], color=cmap(cmap_idx[j]), marker=symbols[i])
+        # ax.set_ylabel("Relative mean measures change")
+        # ax.yaxis.tick_right()
+        ax.set_yticks(ticks=[0, 0.5, 1, 1.5], labels=["MTR", "MTsat", "ihMTR", "ihMTsat"], rotation='vertical', va='center')
+        ax.set_xticks(ticks=np.arange(0, 33, 1) * 3, labels=["AF_L", "AF_R", "CC_1", "CC_2a", "CC_2b", "CC_3", "CC_4",
+                                                            "CC_5", "CC_6", "CC_7", "CG_L", "CG_R", "CR_L", "CR_R",
+                                                            "CST_L", "CST_R", "ICP_L", "ICP_R", "IFOF_L", "IFOF_R",
+                                                            "ILF_L", "ILF_R", "OR_L", "OR_R", "SLF_1_L", "SLF_1_R",
+                                                            "SLF_2_L", "SLF_2_R", "SLF_3_L", "SLF_3_R", "UR_L",
+                                                            "UR_R", "MCP"], rotation='vertical', fontsize=6)
+        from matplotlib.lines import Line2D
+        point1 = Line2D([0], [0], label='max-mean', marker='o', markersize=3, markeredgecolor=cmap(cmap_idx[0]),
+                        markerfacecolor=cmap(cmap_idx[0]), linestyle='')
+        point2 = Line2D([0], [0], label='mean', marker='s', markersize=3, markeredgecolor=cmap(cmap_idx[0]),
+                        markerfacecolor=cmap(cmap_idx[0]), linestyle='')
+        ax.legend(handles=[point1, point2], title="Reference", loc=(0.74, 0.1), handletextpad=0.1)
         # plt.show()
         # plt.subplots_adjust(wspace=2)
         plt.savefig(args.out_filename, dpi=500, bbox_inches='tight')
